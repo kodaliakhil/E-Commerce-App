3080d0e9e982054fa4a4c374224f8b3f1816baf4
“kodaliakhil” <kodaliakhil1999@gmail.com>
2023-05-22 18:07:24 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Home } from './components/Home';
+
+function App() {
+  return (
+    <div>
+      <h1>
+        E-Commerce
+      </h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
