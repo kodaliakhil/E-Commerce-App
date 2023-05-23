73e66e6a3d1989fdcf3a7d554fb97b1ded1106c2
sp-vamshi <spvamshi22@gmail.com>
2023-05-23 11:13:50 +0530

    login created

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Home } from './components/Home';
+import Login from './components/Login';
+
 
 function App() {
   return (
     <div>
       <h1>
         E-Commerce
+        <Login />
       </h1>
       <Home />
     </div>
